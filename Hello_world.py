dedc46e1afd025042a6416115c2bbe850bc6901f
Erik <eriksetterquist@gmail.com>
2018-10-19 20:32:57 +0200

    Added two functions to hello world

--- Hello_world.py
+++ Hello_world.py
@@ -2,3 +2,9 @@ print("Hello world, this is Git!")
 print("now I'm on the master branch!")
 #This is the greatest comment ever!
 #This commen is also cool!
+
+def functionOne():
+    print("this is a function")
+
+def functionTwo():
+    print("this is also a function")
